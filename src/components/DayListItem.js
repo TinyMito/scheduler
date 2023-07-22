a672f2e7b973fb291528ff71a260d5dea16a193e
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-07-22 14:23:46 -0400

    Update DayList with CSS and Test JEST for formatSpots()

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,27 @@
 import React from "react";
+import classNames from "classnames";
+import "./DayListItem.scss";
 
 export default function DayListItem(props) {
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots === 0
+  });
+
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return 'no spots';
+    } else if (props.spots === 1) {
+      return `${props.spots} spot`;
+    } else {
+      return `${props.spots} spots`;
+    }
+  }
+
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots()} remaining</h3>
     </li>
   );
 }
