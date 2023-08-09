261da57eae5bccaf35d6f0ae9efe4b9e9b30f354
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-08-09 16:20:44 -0400

    Update to show Interview name

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-import getAppointmentsForDay from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -26,9 +26,8 @@ export default function Application(props) {
     });
   }, []);
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
-
   const setDay = day => setState({ ...state, day });
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   return (
     <main className="layout">
@@ -53,12 +52,18 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {dailyAppointments.map(appointment => (
-          <Appointment 
-            key={appointment.id} 
-            {...appointment} 
-          />
-        ))}
+        {dailyAppointments.map(appointment => {
+          const interview = getInterview(state, appointment.interview);
+          
+          return (
+            <Appointment 
+              key={appointment.id}
+              id={appointment.id}
+              time={appointment.time}
+              interview={interview}
+            />
+          )
+          })}
       </section>
     </main>
   );
