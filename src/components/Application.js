7834e6ae5b219c4c8dbd78c473cee48483a361cd
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-08-09 15:05:41 -0400

    Combine state

--- src/components/Application.js
+++ src/components/Application.js
@@ -44,17 +44,23 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
   useEffect(() => {
     const daysURL = `http://localhost:8001/api/days`;
     axios.get(daysURL).then(response => {
       console.log(response.data)
-      setDays([...response.data])
+      setDays(response.data)
     });
   }, []);
-  
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -66,8 +72,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
         <DayList
-          days={days}
-          value={day}
+          days={state.days}
+          value={state.day}
           onChange={setDay}
         />
         </nav>
