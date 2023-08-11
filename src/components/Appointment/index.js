b4828e7b3d2ba71260a9d3663b686afa51f4c165
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-08-11 13:26:30 -0400

    Update for custom hooks

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,14 +1,43 @@
 import React from "react";
 import "./styles.scss";
+import Confirm from "./Confirm";
+import Empty from "./Empty";
+import Error from "./Error";
+import Form from "./Form";
 import Header from "./Header";
 import Show from "./Show";
-import Empty from "./Empty";
+import Status from "./Status";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show interview={props.interview} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          interview={props.interview}
+          onEdit={() => console.log("Clicked onEdit")}
+          onDelete={() => console.log("Clicked onDelete")}
+        />
+      )}
+
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={() => back()}
+          onSave={() => console.log("Clicked onSave")}
+          message="Enter Student Name"
+        />
+      )}
     </article>
   )
 }
